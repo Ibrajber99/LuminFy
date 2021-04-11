dc021d5a9a29667bc88db3ae822f8e5be5035b62
Ibrahim jaber <jber595@gmail.com>
2021-04-11 12:14:14 -0300

    D
    D

--- Me_Spotify_App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Me_Spotify_App.MvcApplication" Language="C#" %>
--- Me_Spotify_App/Models/Album_Related/CopyrightModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Me_Spotify_App.Models
-{
-    public class CopyrightModel
-    {
-        public string Text { get; set; }
-        public string Type { get; set; }
-    }
-}
